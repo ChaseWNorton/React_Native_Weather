b49b6bec371c72c680fd3dca896338b95095dec3
ChaseWNorton <chasenorton@me.com>
2018-04-20 14:14:35 -0700

    Static props setup

--- App.js
+++ App.js
@@ -3,19 +3,38 @@ import {
   StyleSheet,
   Text,
   View,
+  ImageBackground,
   KeyboardAvoidingView,
-  TextInput,
   Platform
 } from "react-native";
+import getImageForWeather from "./utils/getImageForWeather";
+import SearchInput from "./components/SearchInput";
 
 export default class App extends React.Component {
   render() {
+    const location = 'San Francisco';
+
     return (
-      <View style={styles.container}>
-        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-        <Text style={[styles.largeText, styles.textStyle]}>24</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <ImageBackground
+          source={getImageForWeather("Clear")}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+
+
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              {location}
+            </Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>24</Text>
+            <SearchInput placeholder="Search Any City" />
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -23,18 +42,33 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#34495E",
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
   textStyle: {
     textAlign: "center",
-    fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto"
+    fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto",
+    color: 'white'
   },
   largeText: {
     fontSize: 44
   },
   smallText: {
     fontSize: 18
-  }
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
 });
